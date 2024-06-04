16ace2d29b413791d8019f119126d6401b11cd7d
MuxaJlbl417 <56254111+MuxaJlbl417@users.noreply.github.com>
2024-06-04 19:13:06 +0500

    Update module_2_3.py

--- module_2_3.py
+++ module_2_3.py
@@ -2,8 +2,8 @@ my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, 5]
 number = 0
 while number < len(my_list):
     number += 1
+    if my_list[number] < 0:
+        break
     if my_list[number] == 0:
         continue
     print(my_list[number])
-    if my_list[number] == 99:
-        break
